cb4a2c1f70507fdc9334d67e974535b32f493531
vikashkumar.dubey <vikashkumar.dubey@LP1-AP-51861903.PROD.HCLPNP.COM>
2020-07-25 13:01:53 +0530

    Dev setup

---
+++ PrototypeModel/src/main/java/com/example/prototype/PrototypeModelApp.java
@@ -0,0 +1,15 @@
+package com.example.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeModelApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PrototypeModelApp.class, args);
+
+	}
+
+}
